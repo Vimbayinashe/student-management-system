5c0a8448164fc2c2fe6692d872ae07b782eb3889
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-24 12:00:51 +0100

    Create StudentNotFoundException and Mapper

--- src/main/java/se/iths/exceptions/StudentNotFoundException.java
+++ src/main/java/se/iths/exceptions/StudentNotFoundException.java
@@ -1,2 +1,18 @@
-package se.iths.exceptions;public class StudentNotFoundException {
+package se.iths.exceptions;
+
+import javax.ws.rs.WebApplicationException;
+
+public class StudentNotFoundException extends WebApplicationException {
+    private Long id;
+
+    public StudentNotFoundException()    {
+    }
+
+    public StudentNotFoundException(Long id)    {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
