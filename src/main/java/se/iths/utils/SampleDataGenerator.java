62a5801f0a8e3fd17fdf5a16145635f96e6c0857
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-24 14:00:46 +0100

    Add sample data generator

---
+++ src/main/java/se/iths/utils/SampleDataGenerator.java
@@ -0,0 +1,33 @@
+package se.iths.utils;
+
+import se.iths.entity.Student;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Singleton
+@Startup
+public class SampleDataGenerator {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @PostConstruct
+    public void generateSampleData() {
+        List<Student> students = List.of(
+            new Student("Mike", "Thomas", "mike@thomasandsons.com"),
+            new Student("Lewis", "Thomas", "mike@thomasandsons.com"),
+            new Student("Elizabeth", "Stewart", "e.stewart@mymail.com"),
+            new Student("Henry", "Thorbjörn", "henry_b@mymail.com"),
+            new Student("Francis", "Everton", "francis@evertonfc.com"),
+            new Student("Theresa", "X", "thesx@ourmail.com"),
+            new Student("Tui", "Jones", "tjones@mymail.com")
+        );
+
+        students.forEach(student -> entityManager.persist(student));
+    }
+}
