c76714a26ce8a86ff01f4ea06779fdf515d9b740
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-25 12:53:56 +0100

    Refactor StudentNotFoundException

--- src/main/java/se/iths/exceptions/StudentNotFoundException.java
+++ src/main/java/se/iths/exceptions/StudentNotFoundException.java
@@ -1,18 +1,17 @@
 package se.iths.exceptions;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class StudentNotFoundException extends WebApplicationException {
-    private Long id;
-
-    public StudentNotFoundException()    {
-    }
 
     public StudentNotFoundException(Long id)    {
-        this.id = id;
+        super(Response.status(Response.Status.NOT_FOUND)
+                .entity(new ErrorMessage(Response.Status.NOT_FOUND, "Student with ID " + id + " not found",
+                        "/api/v1/students/" +id))
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .build());
     }
 
-    public Long getId() {
-        return id;
-    }
 }
