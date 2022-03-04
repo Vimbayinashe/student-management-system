b097372520f34548d3bc2f2ef22c2ea13551f3e2
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-03-04 10:47:17 +0100

    Refactor Default Data

--- src/main/java/se/iths/utils/SampleDataGenerator.java
+++ src/main/java/se/iths/utils/SampleDataGenerator.java
@@ -41,9 +41,12 @@ public class SampleDataGenerator {
 
         students.forEach(student -> subjects.forEach(subject -> subject.addStudent(student)));
 
-        teacher1.addSubjectAndTeacher(subjects.get(0));
-        teacher1.addSubjectAndTeacher(subjects.get(1));
-        teacher2.addSubjectAndTeacher(subjects.get(2));
+        teacher1.addSubject(subjects.get(0));
+        subjects.get(0).setTeacher(teacher1);
+        teacher1.addSubject(subjects.get(1));
+        subjects.get(1).setTeacher(teacher1);
+        teacher2.addSubject(subjects.get(2));
+        subjects.get(2).setTeacher(teacher2);
 
         subjects.forEach(subject -> entityManager.persist(subject));
 
@@ -66,7 +69,7 @@ public class SampleDataGenerator {
         List<Teacher> teachers = List.of(
                 new Teacher("Michael", "Taylor", "taylor_man@myemail.com"),
                 new Teacher("Per", "Simpson", "pers@mymail.com"),
-                new Teacher("Miley", "Sunboom", "miss_moon@mymail.com")
+                new Teacher("Miley", "Sunboom", "miss_moon@mymail.com", "0732345678")
         );
 
         teachers.forEach(teacher -> entityManager.persist(teacher));
